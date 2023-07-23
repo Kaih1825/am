c4bc73cda6f9c5c70223428544dc69487f0a846d
Kai <imatch181@gmail.com>
2023-07-23 16:18:16 +0800

    0723

--- lib/Screens/HomeContainer.dart
+++ lib/Screens/HomeContainer.dart
@@ -2,6 +2,7 @@ import 'package:aa_0718/Screens/HomeScreen.dart';
 import 'package:aa_0718/Screens/Records.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:quick_actions/quick_actions.dart';
 
 import 'TicketsList.dart';
 
@@ -15,6 +16,36 @@ class HomeContainer extends StatefulWidget {
 class _HomeContainerState extends State<HomeContainer> {
   var screens = [const HomeScreen(), const TicketsList(), const Records()];
   var selected = 0;
+  var quickActions = const QuickActions();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    quickActions.setShortcutItems([
+      const ShortcutItem(
+          type: "records", localizedTitle: "Records", icon: "record"),
+      const ShortcutItem(
+          type: "Tickets", localizedTitle: "Tickets", icon: "ticket"),
+      const ShortcutItem(
+          type: "Events", localizedTitle: "Events", icon: "home"),
+    ]);
+
+    quickActions.initialize((type) {
+      switch (type) {
+        case "records":
+          selected = 0;
+          break;
+        case "Tickets":
+          selected = 1;
+          break;
+        case "Events":
+          selected = 2;
+          break;
+      }
+      setState(() {});
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
